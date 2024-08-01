7324ff90ccd0407b61ba992ba34ec513922e2d8f
Wangui <elswangui@gmail.com>
2024-08-01 17:42:52 +0300

    update App.js

---
+++ src/components/Filter.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function Filter({onCategoryChange}){
+    function handleSelectChange(event) {
+        onCategoryChange(event.target.value);
+      }
+    
+    return(
+        
+        <div className="Filter">
+        <select name="filter" onChange={handleSelectChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+
+       
+
+    )
+}
+export default Filter
